19e7a929271919db669fd34aeafe51090449928c
saifulkhan <saiful.etc@gmail.com>
2024-10-18 15:08:54 +0000

    update M2 model

--- src/trainer_M1/ibm_trainer_Fixed.py
+++ src/trainer_M1/ibm_trainer_Fixed.py
@@ -53,12 +53,12 @@ class Trainer(BaseTrainer):
         total_loss = sum(
             [
                 2.0 * ((bclosses["left"])),
-                2.0 * ((bclosses["right"])),
-                2.0 * ((bclosses["bottom"])),
-                2.0 * ((bclosses["up"])),
+                # 2.0 * ((bclosses["right"])),
+                # 2.0 * ((bclosses["bottom"])),
+                # 2.0 * ((bclosses["up"])),
                 4.0 * ((bclosses["fluid_points"])),
-                2.0 * ((bclosses["initial"])),
-                0.01 * ((bclosses["fluid"])),
+                # 2.0 * ((bclosses["initial"])),
+                0.005 * ((bclosses["fluid"])),
             ]
         )
 
@@ -217,11 +217,11 @@ class Trainer(BaseTrainer):
         ## presssure training is necessary
 
         return {
-            "left": lleft,
+            "left": lleft + lright + lbottom + lup,
             "right": lright,
             "bottom": lbottom,
             "up": lup,
-            "fluid_points": lsensors,
+            "fluid_points": lsensors + linitial,
             "initial": linitial,
             "fluid": lphy,
         }
